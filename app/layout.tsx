c09dabea7384479f7b3a772b562df8b13f3d11e4
Hugo Lucero <h.lucero@itecriocuarto.org.ar>
2026-06-03 16:49:20 -0300

    update logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,20 +12,7 @@ export const metadata: Metadata = {
   description: 'Panel de control de gestión de pedidos, stock, artículos y revendedores',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
+    icon: '/isologo.png',
     apple: '/apple-icon.png',
   },
 }
